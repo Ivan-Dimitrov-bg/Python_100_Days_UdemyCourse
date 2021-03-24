04c329065252b76ab3c26e213250c055be817e1a
Ivan Dimitrov <Ivan.Dimitrov.bg@gmail.com>
2021-03-24 09:08:12 +0200

    Day 39

--- 39.intermediateCapstoneFlightDealFinder/flight-deals-start/flight_search.py
+++ 39.intermediateCapstoneFlightDealFinder/flight-deals-start/flight_search.py
@@ -1,3 +1,16 @@
+import requests
+import os
+
+TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
+TEQUILA_API_KEY = os.environ["tequila_key"]
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+
+    def get_destination_code(self, city_name):
+        location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {"term": city_name, "location_types": "city"}
+        response = requests.get(url=location_endpoint, headers=headers, params=query)
+        results = response.json()["locations"]
+        code = results[0]["code"]
+        return code
